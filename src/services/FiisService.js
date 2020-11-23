e91ba7eb62ddd858b4ec6ff1509d1549860c99fe
Crisciano da Silva Botelho <crisciano.botelho@pampa.compasso>
2020-11-23 09:47:46 -0300

    Solved puppeter heroku

--- src/services/FiisService.js
+++ src/services/FiisService.js
@@ -4,7 +4,12 @@ class FiisServices {
 
     getFiis = () => {
         return new Promise(async (resolve, reject)  => {
-            const browser = await puppeteer.launch();
+            const browser = await puppeteer.launch({
+                args: [
+                  '--no-sandbox',
+                  '--disable-setuid-sandbox',
+                ],
+            });
             const page = await browser.newPage()
             await page.goto(process.env.URL)
             
