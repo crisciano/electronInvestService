21f6bf9a8adb92ac7dce010927c1afaaca96eb88
Crisciano S. Botelho <crisciano.botelho@compasso.com.br>
2022-09-05 12:02:40 -0300

    Include new information

--- src/config/config.js
+++ src/config/config.js
@@ -23,8 +23,15 @@ module.exports = {
             "lucros_Cagr5", 
             "vpa",
             "lpa",
+            "margemLiquida",
             "liquidezMediaDiaria"
         ] 
     }
 
 }
+
+// ações keys
+// ticker,price,p_L,dy,p_VP,p_Ebit,p_Ativo,eV_Ebit,margemBruta,margemEbit,margemLiquida,p_SR,p_CapitalGiro,p_AtivoCirculante,giroAtivos,roe,roa,roic,dividaliquidaPatrimonioLiquido,dividaLiquidaEbit,pl_Ativo,passivo_Ativo,liquidezCorrente,peg_Ratio,receitas_Cagr5,lucros_Cagr5,liquidezMediaDiaria,vpa,lpa,valorMercado
+
+//fiis keys
+// id,setor,preco,liquidez,dividendo,DY,DY_acum_3m,DY_acum_6m,DY_acum_12m,DY_medio_3m,DY_medio_6m,DY_medio_12m,DY_ano,variacao_preco,rentabilidade_periodo,rentabilidade_acumulada,patrimonio_liq,VPA,P_VPA,DY_patrimonial,variacao_patrimonial,rentabilidade_patrimonial_periodo,rentabilidade_patrimonial_acumulada,vacancia_fisica,vacancia_financeira,quantidade_ativos,
