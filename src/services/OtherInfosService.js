3c490c607f21bbabdafd761f18ccebc5ec5e7823
Crisciano S. Botelho <crisciano.botelho@compasso.com.br>
2023-04-10 16:54:01 -0300

    config use-agent in axios

--- src/services/OtherInfosService.js
+++ src/services/OtherInfosService.js
@@ -1,38 +1,38 @@
-const axios = require("axios")
-const { logger } = require("../utils/logger")
-const { genericError } = require("../utils/Message")
-
+const axios = require("axios");
+const { logger } = require("../utils/logger");
+const { genericError } = require("../utils/Message");
 
 class OtherInfosService {
+  getFiisFull = (search) => {
+    return new Promise((resolve, reject) => {
+      try {
+        // CategoryType 2 = fiis 1 = ações
+        const options = {
+          method: "get",
+          url: process.env.OTHER_URL + "/category/advancedsearchresult",
+          params: {
+            search:
+              '{"Segment":"","Gestao":"","my_range":"0;20","dy":{"Item1":null,"Item2":null},"p_vp":{"Item1":null,"Item2":null},"percentualcaixa":{"Item1":null,"Item2":null},"numerocotistas":{"Item1":null,"Item2":null},"dividend_cagr":{"Item1":null,"Item2":null},"cota_cagr":{"Item1":null,"Item2":null},"liquidezmediadiaria":{"Item1":null,"Item2":null},"patrimonio":{"Item1":null,"Item2":null}}',
+            CategoryType: 2,
+          },
+          headers: {
+            "User-Agent": "null",
+          },
+        };
 
-    getFiisFull = (search) => {
-        return new Promise((resolve, reject) => {
-
-            try {
-                // CategoryType 2 = fiis 1 = ações
-                const options = {
-                    method: 'get',
-                    url: process.env.OTHER_URL + "/category/advancedsearchresult",
-                    params: {
-                        search: search ? search : '{"Segment":"","Gestao":"","my_range":"0;20","dy":{"Item1":null,"Item2":null},"p_vp":{"Item1":null,"Item2":null},"percentualcaixa":{"Item1":null,"Item2":null},"numerocotistas":{"Item1":null,"Item2":null},"dividend_cagr":{"Item1":null,"Item2":null},"cota_cagr":{"Item1":null,"Item2":null},"liquidezmediadiaria":{"Item1":null,"Item2":null},"patrimonio":{"Item1":null,"Item2":null}}',
-                        CategoryType: 2
-
-                    }
-                }
-
-                axios(options)
-                    .then(res => resolve(res.data))
-                    .catch(err => {
-                        logger.error(genericError('getFiisFull - axios', err))
-                        reject(err)
-                    })
-            } catch (error) {
-                logger.error(genericError('getFiisFull', err))
-                reject(err)
-            }
-        })
-    }
-
+        axios(options)
+          .then((res) => resolve(res.data))
+          .catch((err) => {
+            console.log(err);
+            logger.error(genericError("getFiisFull - axios", err));
+            reject(err);
+          });
+      } catch (error) {
+        logger.error(genericError("getFiisFull", err));
+        reject(err);
+      }
+    });
+  };
 }
 
-module.exports = new OtherInfosService()
+module.exports = new OtherInfosService();
